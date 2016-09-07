6d4f345118a4c3528eb59482cd75b91ccfb6e4a1
huangziyi01 <huangziyi01@baidu.com>
2016-09-07 17:53:43 +0800

    发布组件

---
+++ lib/index.d.ts
@@ -0,0 +1,4 @@
+import TransmitTransparently from './transmit-transparently/transmit-transparently.component';
+import others from './others/others';
+export { TransmitTransparently, others };
+export default TransmitTransparently;
---
+++ lib/others/others.d.ts
@@ -0,0 +1,2 @@
+declare var _default: (defaultProps?: any, props?: any, ignore?: string[], isReactNative?: boolean) => any;
+export default _default;
